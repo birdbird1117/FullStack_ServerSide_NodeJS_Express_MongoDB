20ceb08115205b57ee9141da400a7eca536f3a97
birdbird1117 <dapeng.han.1989@gmail.com>
2017-10-26 23:19:03 +0200

    week2 assignment promotions

--- NodeJS/conFusionServer/routes/promoRouter.js
+++ NodeJS/conFusionServer/routes/promoRouter.js
@@ -44,19 +44,38 @@ promoRouter.route('/')
 
 promoRouter.route('/:promoId')
 .get((req,res,next) => {
-    res.end('Will send details of the promotion: ' + req.params.promoId +' to you!');
+    Promotions.findById(req.params.promoId)
+    .then((promotion) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
 .put((req, res, next) => {
-    res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + 
-        ' with details: ' + req.body.description);
+    Promotions.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body
+    }, { new: true })
+    .then((promotion) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .delete((req, res, next) => {
-    res.end('Deleting promotion: ' + req.params.promoId);
+    Promotions.findByIdAndRemove(req.params.promoId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
+
 
 module.exports = promoRouter;
